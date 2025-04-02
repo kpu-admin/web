0682834eec75b2458f3fa8135efad1d9427e3d84
lmx <2538381255@qq.com>
2025-04-03 06:59:27 +0800

    依赖升级

--- src/router/modules/external.link.example.ts
+++ src/router/modules/external.link.example.ts
@@ -13,77 +13,96 @@ const routes: RouteRecordRaw = {
     title: '外链',
     icon: 'i-ri:external-link-fill',
   },
-  children: [{
-    path: 'iframe',
-    redirect: '/link/iframe/iframeVue',
-    meta: {
-      title: '内嵌 Iframe',
+  children: [
+    {
+      path: 'iframe',
+      redirect: '/link/iframe/iframeVue',
+      meta: {
+        title: '内嵌 Iframe',
+      },
+      children: [
+        {
+          path: 'iframeVue',
+          redirect: '',
+          name: 'linkExampleIframeVue',
+          meta: {
+            title: 'Vue 官方文档',
+            iframe: 'https://cn.vuejs.org/',
+            cache: true,
+            copyright: false,
+          },
+        },
+        {
+          path: 'iframeVueRouter',
+          redirect: '',
+          name: 'linkExampleIframeVueRouter',
+          meta: {
+            title: 'Vue-Router 官方文档',
+            iframe: 'https://router.vuejs.org/zh/',
+            cache: true,
+            copyright: false,
+          },
+        },
+        {
+          path: 'iframeVuePinia',
+          redirect: '',
+          name: 'linkExampleIframeVuePinia',
+          meta: {
+            title: 'Pinia 官方文档',
+            iframe: 'https://pinia.vuejs.org/zh/',
+            cache: true,
+            copyright: false,
+          },
+        },
+        {
+          path: 'iframeElement',
+          redirect: '',
+          name: 'linkExampleIframeElement',
+          meta: {
+            title: 'Element Plus 官方文档',
+            iframe: 'https://element-plus.org/zh-CN/',
+            cache: true,
+            copyright: false,
+          },
+        },
+        {
+          path: 'iframeAntdv',
+          redirect: '',
+          name: 'linkExampleIframeAntdv',
+          meta: {
+            title: 'Ant Design of Vue 官方文档',
+            iframe: 'https://www.antdv.com/docs/vue/introduce-cn/',
+            cache: true,
+            copyright: false,
+          },
+        },
+      ],
     },
-    children: [{
-      path: 'iframeVue',
-      redirect: '',
-      name: 'linkExampleIframeVue',
+    {
+      path: 'window',
+      redirect: '/link/window/gitee',
       meta: {
-        title: 'Vue 官方文档',
-        iframe: 'https://cn.vuejs.org/',
-        cache: true,
-        copyright: false,
+        title: '新窗口打开',
       },
-    }, {
-      path: 'iframeVueRouter',
-      redirect: '',
-      name: 'linkExampleIframeVueRouter',
-      meta: {
-        title: 'Vue-Router 官方文档',
-        iframe: 'https://router.vuejs.org/zh/',
-        cache: true,
-        copyright: false,
-      },
-    }, {
-      path: 'iframeVuePinia',
-      redirect: '',
-      name: 'linkExampleIframeVuePinia',
-      meta: {
-        title: 'Pinia 官方文档',
-        iframe: 'https://pinia.vuejs.org/zh/',
-        cache: true,
-        copyright: false,
-      },
-    }, {
-      path: 'iframeElement',
-      redirect: '',
-      name: 'linkExampleIframeElement',
-      meta: {
-        title: 'Element Plus 官方文档',
-        iframe: 'https://element-plus.org/zh-CN/',
-        cache: true,
-        copyright: false,
-      },
-    }],
-  }, {
-    path: 'window',
-    redirect: '/link/window/gitee',
-    meta: {
-      title: '新窗口打开',
+      children: [{
+        path: 'gitee',
+        redirect: '',
+        name: 'linkExampleWindowGitee',
+        meta: {
+          title: 'Gitee 仓库',
+          link: 'https://gitee.com/muxian/vue3-ts',
+        },
+      }, {
+        path: 'github',
+        redirect: '',
+        name: 'linkExampleWindowGithub',
+        meta: {
+          title: 'Github 仓库',
+          link: 'https://github.com/Lmx1220/vue3-ts',
+        },
+      }],
     },
-    children: [{
-      path: 'gitee',
-      redirect: '',
-      name: 'linkExampleWindowGitee',
-      meta: {
-        title: 'Gitee 仓库',
-        link: 'https://gitee.com/muxian/vue3-ts',
-      },
-    }, {
-      path: 'github',
-      redirect: '',
-      name: 'linkExampleWindowGithub',
-      meta: {
-        title: 'Github 仓库',
-        link: 'https://github.com/Lmx1220/vue3-ts',
-      },
-    }],
-  }],
+  ],
 }
 
 export default routes
